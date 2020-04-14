7e997539a3c76387bb9ff63440db265cb63d061b
Humberto Arana <humaranah@gmail.com>
2020-04-13 20:22:46 -0500

    Add logs to ps1

--- script/build.ps1
+++ script/build.ps1
@@ -1,4 +1,7 @@
+Write-Host 'Getting current version file'
 $version = Get-Content '.\version.txt';
+Write-Host "Current version: $version"
+
 &dotnet build --configuration Release --no-restore /p:Version=$version;
 
 exit 0
