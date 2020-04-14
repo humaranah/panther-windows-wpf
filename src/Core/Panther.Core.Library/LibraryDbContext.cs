fd1fc6d653fa7a575503637ec275ae7e7822bc12
Humberto Arana <humaranah@gmail.com>
2020-04-14 13:56:42 -0500

    Add entity framework to sqlite in library project

---
+++ src/Core/Panther.Core.Library/LibraryDbContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Panther.Core.Models;
+
+namespace Panther.Core.Library
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=library.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>()
+                .HasKey(song => song.Id);
+            modelBuilder.Entity<Song>()
+                .HasIndex(song => song.FileName)
+                .IsUnique();
+            modelBuilder.Entity<Song>()
+                .HasOne(song => song.Album)
+                .WithMany(album => album.Songs)
+                .HasForeignKey(song => song.AlbumId);
+            modelBuilder.Entity<Song>()
+                .HasOne(song => song.Artist)
+                .WithMany(artist => artist.Songs)
+                .HasForeignKey(song => song.ArtistId);
+
+            modelBuilder.Entity<Album>()
+                .HasKey(album => album.Id);
+            modelBuilder.Entity<Album>()
+                .HasIndex(album => album.Name)
+                .IsUnique();
+            modelBuilder.Entity<Album>()
+                .HasOne(album => album.Artist)
+                .WithMany(artist => artist.Albums)
+                .HasForeignKey(album => album.ArtistId);
+
+            modelBuilder.Entity<Artist>()
+                .HasKey(artist => artist.Id);
+            modelBuilder.Entity<Artist>()
+                .HasIndex(artist => artist.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Playlist>()
+                .HasKey(playlist => playlist.Id);
+
+            modelBuilder.Entity<SongPlaylist>()
+                .HasKey(songPlaylist => new { songPlaylist.SongId, songPlaylist.PlaylistId });
+            modelBuilder.Entity<SongPlaylist>()
+                .HasOne(songPlaylist => songPlaylist.Song)
+                .WithMany(song => song.SongPlaylists)
+                .HasForeignKey(songPlaylist => songPlaylist.SongId);
+            modelBuilder.Entity<SongPlaylist>()
+                .HasOne(songPlaylist => songPlaylist.Playlist)
+                .WithMany(playlist => playlist.SongPlaylists)
+                .HasForeignKey(songPlaylist => songPlaylist.PlaylistId);
+        }
+    }
+}
---
+++ src/Core/Panther.Core/Models/IIdentificable.cs
@@ -0,0 +1,8 @@
+﻿namespace Panther.Core.Models
+{
+    public interface IIdentificable
+    {
+        long Id { get; set; }
+        string Name { get; set; }
+    }
+}
