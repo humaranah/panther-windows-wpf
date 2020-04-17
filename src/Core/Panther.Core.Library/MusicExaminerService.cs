531016143d3451f4c7545c94a0c29c2a2d62b402
humaranah <humaranah@gmail.com>
2020-04-16 22:51:05 -0500

    Add music examiner service
    Co-Authored-By: Humberto Arana <humaranah@users.noreply.github.com>

---
+++ src/Core/Panther.Core.Library/MusicExaminerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Panther.Core.Library
+{
+    // TODO implement using observable pattern
+    public class MusicExaminerService : IMusicExaminerService
+    {
+        public IEnumerable<Task> ExamineAsync(string path, Func<string, Task> asyncFileAction)
+        {
+            var directoryInfo = new DirectoryInfo(path);
+
+            var files = directoryInfo
+                .GetFiles("*", SearchOption.AllDirectories)
+                .Where(file => file.Attributes == (FileAttributes.Normal | FileAttributes.ReadOnly));
+
+            foreach (var file in files)
+            {
+                yield return asyncFileAction(file.FullName);
+            }
+        }
+
+        public IEnumerable<Task> ExamineAsync(IEnumerable<string> paths, Func<string, Task> asyncFileAction)
+            => paths.SelectMany(path => ExamineAsync(path, asyncFileAction));
+    }
+}
---
+++ src/Core/Panther.Core/Library/IMusicExaminerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Panther.Core.Library
+{
+    public interface IMusicExaminerService
+    {
+        IEnumerable<Task> ExamineAsync(string path, Func<string, Task> asyncFileAction);
+        IEnumerable<Task> ExamineAsync(IEnumerable<string> paths, Func<string, Task> asyncFileAction);
+    }
+}
