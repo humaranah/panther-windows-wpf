2065436496997341a9525f3701c25e1f317d453b
Humberto Arana <humaranah@gmail.com>
2020-05-16 17:40:24 -0500

    Add mini player window

---
+++ src/Desktop/Windows/Panther.Windows.Views/MiniPlayerWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Panther.Windows.Views.ViewModels;
+using System.Linq;
+using System.Windows;
+
+namespace Panther.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for MiniPlayerWindow.xaml
+    /// </summary>
+    public partial class MiniPlayerWindow : Window
+    {
+        private MiniPlayerViewModel ViewModel => DataContext as MiniPlayerViewModel;
+
+        public MiniPlayerWindow(MiniPlayerViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        private void OnFileDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return;
+            }
+
+            var file = (e.Data.GetData(DataFormats.FileDrop) as string[])
+                    .First();
+            ViewModel.LoadFile(file);
+        }
+    }
+}
---
+++ src/Desktop/Windows/Panther.Windows.Views/ViewModels/MiniPlayerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using Panther.Core.Player;
+
+namespace Panther.Windows.Views.ViewModels
+{
+    public class MiniPlayerViewModel : ViewModelBase
+    {
+        private IPlayerService _player;
+        private IQueueService _queue;
+
+        public MiniPlayerViewModel(IPlayerService player, IQueueService queue) : base()
+        {
+            _player = player;
+            _queue = queue;
+        }
+
+        public void LoadFile(string fileName)
+        {
+            _player.Load(fileName);
+        }
+    }
+}
