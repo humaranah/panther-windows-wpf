647aff6fa0aa36b1c9d7f19ade19ed2e8bc83fd0
Humberto Arana <humaranah@gmail.com>
2020-06-19 16:28:29 -0500

    Add converters to desktop view

---
+++ src/Desktop/Windows/Panther.Windows.Views/Converters/TagLibPictureToBitmapSourceConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using TagLib;
+
+namespace Panther.Windows.Views.Converters
+{
+    public class TagLibPictureToBitmapSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is IPicture picture))
+            {
+                return null;
+            }
+
+            var image = new BitmapImage();
+
+            using (var stream = new MemoryStream(picture.Data.Data))
+            {
+                stream.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = stream;
+                image.EndInit();
+            }
+
+            image.Freeze();
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is BitmapSource source))
+            {
+                return null;
+            }
+
+            var encoder = new JpegBitmapEncoder
+            {
+                QualityLevel = 100
+            };
+
+            using var stream = new MemoryStream();
+            encoder.Frames.Add(BitmapFrame.Create(source));
+            encoder.Save(stream);
+            var bytes = stream.ToArray();
+            return new Picture(new ByteVector(bytes, bytes.Length));
+        }
+    }
+}
