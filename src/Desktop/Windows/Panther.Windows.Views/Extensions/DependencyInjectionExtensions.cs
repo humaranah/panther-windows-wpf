575d5fed5a6f155c9026a87fccdbcc6a306f7830
Humberto Arana <humaranah@gmail.com>
2020-06-19 16:29:10 -0500

    Add dispatcher timer to desktop views

--- src/Desktop/Windows/Panther.Windows.Views/Extensions/DependencyInjectionExtensions.cs
+++ src/Desktop/Windows/Panther.Windows.Views/Extensions/DependencyInjectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Panther.Windows.Views;
 using Panther.Windows.Views.ViewModels;
+using System;
+using System.Windows.Threading;
 
 namespace Panther.NetCore.Extensions
 {
@@ -8,12 +10,20 @@ namespace Panther.NetCore.Extensions
     {
         public static IServiceCollection AddViews(this IServiceCollection services) =>
             services
+                .AddPlaybackTimer()
                 .AddWindows();
 
         private static IServiceCollection AddWindows(this IServiceCollection services) =>
             services
                 .AddSingleton<MainWindow>()
                 .AddSingleton<MiniPlayerWindow>()
-                .AddSingleton<MiniPlayerViewModel>();
+                .AddSingleton<IPlayerViewModel, PlayerViewModel>();
+
+        private static IServiceCollection AddPlaybackTimer(this IServiceCollection services) =>
+            services
+                .AddSingleton<DispatcherTimer>(new DispatcherTimer(DispatcherPriority.DataBind)
+                {
+                    Interval = TimeSpan.FromMilliseconds(1000),
+                });
     }
 }
